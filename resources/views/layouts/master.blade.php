0d3a5e99e372ccb751e033bb1322f8ac82aca249
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-08-31 17:51:19 +0700

    select-data

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -123,222 +123,198 @@
     <script src="{{asset('assets/plugins/dropify/dist/js/dropify.min.js')}}"></script>
     <!-- for Date / time use moment.js -->
     <script src="{{asset('assets/plugins/moment/moment.js')}}"></script>
+    <!-- for selected data -->
+    <script src="{{asset('assets/js/select-data.js')}}"></script>
 
     <script type="text/javascript">
+        function city_Selectdata(values) {
+            var data = $.ajax({
+                url: "{{ url('/city/dataSelect') }}",
+                type: "GET",
+                data: { }
+            })
+            .done(function(data) {
+                var obj = jQuery.parseJSON(data);
+                arrs = [];
+                for (var i = 0; i<obj.length; i++) {
+                    arrs.push( {
+                        id : obj[i].id,
+                        text : obj[i].text
+                    } );
+                }
+                var area_id = $("area_id").val();
+                $("#city_id").select2({
+                    multiple: false,
+                    placeholder: 'Choose City',
+                    tags: true,
+                    data : arrs
+                }).on('change', function (e) {
+                    var city_id = $(this).val();
+                    area_Selectdata(city_id);
 
-    function city_Selectdata(values) {
-        var data = $.ajax({
-            url: "{{ url('/city/dataSelect') }}",
-            type: "GET",
-            data: { }
-        })
-        .done(function(data) {
-            var obj = jQuery.parseJSON(data);
-            arrs = [];
-            for (var i = 0; i<obj.length; i++) {
-                arrs.push( {
-                    id : obj[i].id,
-                    text : obj[i].text
-                } );
-            }
-            var area_id = $("area_id").val();
-            $("#city_id").select2({
-                multiple: false,
-                placeholder: 'Choose City',
-                tags: true,
-                data : arrs
-            }).on('change', function (e) {
-                var city_id = $(this).val();
-                area_Selectdata(city_id);
-
+                });
+                if(values != ''){
+                    $("#city_id").val(values).trigger('change');
+                    area_Selectdata(values, area_id);
+                }
             });
+
             if(values != ''){
                 $("#city_id").val(values).trigger('change');
                 area_Selectdata(values, area_id);
             }
-        });
-
-        if(values != ''){
-            $("#city_id").val(values).trigger('change');
-            area_Selectdata(values, area_id);
         }
-    }
-    city_Selectdata('');
+        city_Selectdata('');
 
-    @if(isset($box->area_id))
-      area_Selectdata({{$box->area_id}}, '');
-    @endif
+        @if(isset($warehouse->area_id))
+          area_Selectdata({{$warehouse->area_id}}, '');
+        @endif
 
-    @if(isset($space->area_id))
-      area_Selectdata({{$space->area_id}}, '');
-    @endif
+        @if(isset($box->area_id))
+          area_Selectdata({{$box->area_id}}, '');
+        @endif
 
-    @if(isset($room->area_id))
-      area_Selectdata({{$room->area_id}}, '');
-    @endif
+        @if(isset($space->area_id))
+          area_Selectdata({{$space->area_id}}, '');
+        @endif
 
-    function area_Selectdata(city_id, values) {
-        var data = $.ajax({
-            url: "{{ url('/area/dataSelect/') }}/"+city_id,
-            type: "GET",
-            data: { }
-        })
-        .done(function(data) {
+        @if(isset($room->area_id))
+          area_Selectdata({{$room->area_id}}, '');
+        @endif
 
-            var obj = jQuery.parseJSON(data);
-            arrs = [];
-            for (var i = 0; i<obj.length; i++) {
-                arrs.push( {
-                    id : obj[i].id,
-                    text : obj[i].text
-                } );
-            }
-            var area_id = $("#area_id").val();
-            $("#area_id").select2({
-                multiple: false,
-                placeholder: 'Choose Area',
-                tags: true,
-                data : arrs
-            }).on('change', function (e) {
-                var area_id = $(this).val();
-                warehouse_Selectdata(area_id);
+        function area_Selectdata(city_id, values) {
+            var data = $.ajax({
+                url: "{{ url('/area/dataSelect/') }}/"+city_id,
+                type: "GET",
+                data: { }
             })
+            .done(function(data) {
+
+                var obj = jQuery.parseJSON(data);
+                arrs = [];
+                for (var i = 0; i<obj.length; i++) {
+                    arrs.push( {
+                        id : obj[i].id,
+                        text : obj[i].text
+                    } );
+                }
+                var area_id = $("#area_id").val();
+                $("#area_id").select2({
+                    multiple: false,
+                    placeholder: 'Choose Area',
+                    tags: true,
+                    data : arrs
+                }).on('change', function (e) {
+                    var area_id = $(this).val();
+                    warehouse_Selectdata(area_id);
+                })
 
 
+                if(values != ''){
+                    $("#area_id").val(values).trigger("change");
+                    warehouse_Selectdata(area_id);
+                }
+            });
             if(values != ''){
                 $("#area_id").val(values).trigger("change");
                 warehouse_Selectdata(area_id);
             }
-        });
-        if(values != ''){
-            $("#area_id").val(values).trigger("change");
-            warehouse_Selectdata(area_id);
         }
-    }
 
-    // function area_Selectdata(city_id, values) {
-    //     $('#area_id').select2({
-    //         ajax: {
-    //             url: "{{ url('/area/dataSelect') }}",
-    //         }
-    //     });
+        @if(isset($box->area_id))
+          warehouse_Selectdata({{$box->area_id}});
+        @endif
 
-    //     var areaSelect = $('#area_id');
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: "{{ url('/area/dataSelect/') }}/"+city_id,
-    //         data: { }
-    //     }).then(function (data) {
-    //         // create the option and append to Select2
-    //         var option = new Option(data.id, true, true);
-    //         areaSelect.append(option).trigger('change');
+        @if(isset($space->area_id))
+          warehouse_Selectdata({{$space->area_id}});
+        @endif
 
-    //         // manually trigger the `select2:select` event
-    //         areaSelect.trigger({
-    //             type: 'select2:select',
-    //             params: {
-    //                 data: data
-    //             }
-    //         });
-    //     });
+        @if(isset($room->area_id))
+          warehouse_Selectdata({{$room->area_id}});
+        @endif
 
-    // }
+        function warehouse_Selectdata(area_id) {
+            var $id = area_id;
+            var data = $.ajax({
+                url: "{{ url('/warehouse/dataSelect/') }}/"+area_id,
+                type: "GET",
+                data: { }
+            })
+            .done(function(data) {
+                var obj = jQuery.parseJSON(data);
+                arrs = [];
+                for (var i = 0; i<obj.length; i++) {
+                    arrs.push( {
+                        id : obj[i].id,
+                        text : obj[i].text
+                    } );
+                }
+                var warehouse_id = $("#warehouse_id").val();
+                $("#warehouse_id").select2({
+                    multiple: false,
+                    placeholder: 'Choose Warehouse',
+                    tags: true,
+                    data : arrs
+                }).on('change', function (e) {
+                    var warehouse_id = $(this).val();
+                    space_Selectdata(warehouse_id);
+                });
 
-    @if(isset($box->area_id))
-      warehouse_Selectdata({{$box->area_id}});
-    @endif
 
-    @if(isset($space->area_id))
-      warehouse_Selectdata({{$space->area_id}});
-    @endif
-
-    @if(isset($room->area_id))
-      warehouse_Selectdata({{$room->area_id}});
-    @endif
-
-    function warehouse_Selectdata(area_id) {
-        var $id = area_id;
-        var data = $.ajax({
-            url: "{{ url('/warehouse/dataSelect/') }}/"+area_id,
-            type: "GET",
-            data: { }
-        })
-        .done(function(data) {
-            var obj = jQuery.parseJSON(data);
-            arrs = [];
-            for (var i = 0; i<obj.length; i++) {
-                arrs.push( {
-                    id : obj[i].id,
-                    text : obj[i].text
-                } );
-            }
-            var warehouse_id = $("#warehouse_id").val();
-            $("#warehouse_id").select2({
-                multiple: false,
-                placeholder: 'Choose Warehouse',
-                tags: true,
-                data : arrs
-            }).on('change', function (e) {
-                var warehouse_id = $(this).val();
-                space_Selectdata(warehouse_id);
+                if(warehouse_id != ""){
+                    $("#warehouse_id").val(warehouse_id).trigger("change");
+                    space_Selectdata(warehouse_id);
+                }else{
+                    $("#warehouse_id").val('').trigger("change");
+                }
             });
+        }
+
+        @if(isset($box->warehouse_id))
+          space_Selectdata({{$box->warehouse_id}});
+        @endif
+
+        @if(isset($space->warehouse_id))
+          space_Selectdata({{$space->warehouse_id}});
+        @endif
+
+        @if(isset($room->warehouse_id))
+          space_Selectdata({{$room->warehouse_id}});
+        @endif
+
+        function space_Selectdata(warehouse_id) {
+            var data = $.ajax({
+                url: "{{ url('/space/dataSelect/') }}/"+warehouse_id,
+                type: "GET",
+                data: { }
+            })
+            .done(function(data) {
+                var obj = jQuery.parseJSON(data);
+                arrs = [];
+                for (var i = 0; i<obj.length; i++) {
+                    arrs.push( {
+                        id : obj[i].id,
+                        text : obj[i].text
+                    } );
+                }
+                var space_id = $("#space_id").val();
+                $("#space_id").select2({
+                    multiple: false,
+                    placeholder: 'Choose Space',
+                    tags: true,
+                    data : arrs
+                }).on('change', function (e) {
+                    var space_id = $(this).val();
+                });
 
 
-            if(warehouse_id != ""){
-                $("#warehouse_id").val(warehouse_id).trigger("change");
-                space_Selectdata(warehouse_id);
-            }else{
-                $("#warehouse_id").val('').trigger("change");
-            }
-        });
-    }
-
-    @if(isset($box->warehouse_id))
-      space_Selectdata({{$box->warehouse_id}});
-    @endif
-
-    @if(isset($space->warehouse_id))
-      space_Selectdata({{$space->warehouse_id}});
-    @endif
-
-    @if(isset($room->warehouse_id))
-      space_Selectdata({{$room->warehouse_id}});
-    @endif
-
-    function space_Selectdata(warehouse_id) {
-        var data = $.ajax({
-            url: "{{ url('/space/dataSelect/') }}/"+warehouse_id,
-            type: "GET",
-            data: { }
-        })
-        .done(function(data) {
-            var obj = jQuery.parseJSON(data);
-            arrs = [];
-            for (var i = 0; i<obj.length; i++) {
-                arrs.push( {
-                    id : obj[i].id,
-                    text : obj[i].text
-                } );
-            }
-            var space_id = $("#space_id").val();
-            $("#space_id").select2({
-                multiple: false,
-                placeholder: 'Choose Space',
-                tags: true,
-                data : arrs
-            }).on('change', function (e) {
-                var space_id = $(this).val();
+                if(space_id != ""){
+                    $("#space_id").val(space_id).trigger("change");
+                }else{
+                    $("#space_id").val('').trigger("change");
+                }
             });
-
-
-            if(space_id != ""){
-                $("#space_id").val(space_id).trigger("change");
-            }else{
-                $("#space_id").val('').trigger("change");
-            }
-        });
-    }
-
+        }
     </script>
 
     @yield('close_html')
