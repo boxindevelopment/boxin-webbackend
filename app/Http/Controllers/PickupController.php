50858d7288e63fb6ef1121cfc963b17c095750cc
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-12-10 11:18:26 +0700

    change start date stored box
    - start_date dimulai dari tanggal ketika admin mengubah status pickup_order menjadi 'finished'
    - change tulisan button di edit tipe size

--- app/Http/Controllers/PickupController.php
+++ app/Http/Controllers/PickupController.php
@@ -7,6 +7,7 @@ use App\Model\PickupOrder;
 use App\Model\Order;
 use App\Model\OrderDetail;
 use DB;
+use Carbon\Carbon;
 use App\Repositories\PickupOrderRepository;
 
 class PickupController extends Controller
@@ -63,6 +64,23 @@ class PickupController extends Controller
         for ($i = 0; $i < $count; $i++) {
             $order_detail            = OrderDetail::find($order_details[$i]->id);
             $order_detail->status_id =  $status == '12' ? '4' : $status;
+            $order_detail->start_date= Carbon::now();
+            if($order_detail->status_id == '4' || $order_detail->status_id == 4) {
+                // daily
+                if ($order_detail->types_of_duration_id == 1 || $order_detail->types_of_duration_id == '1') {
+                    $order_detail->end_date     = date('Y-m-d', strtotime('+'.$order_detail->duration.' days', strtotime($order_detail->start_date)));
+
+                }
+                // weekly
+                else if ($order_detail->types_of_duration_id == 2 || $order_detail->types_of_duration_id == '2') {
+                    $end_date                   = $order_detail->duration*7;
+                    $order_detail->end_date     = date('Y-m-d', strtotime('+'.$end_date.' days', strtotime($order_detail->start_date)));
+                }
+                // monthly
+                else if ($order_detail->types_of_duration_id == 3 || $order_detail->types_of_duration_id == '3') {
+                    $order_detail->end_date     = date('Y-m-d', strtotime('+'.$order_detail->duration.' month', strtotime($order_detail->start_date)));
+                }
+            }
             $order_detail->save();
         }
 
