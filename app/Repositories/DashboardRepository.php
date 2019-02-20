c6c7aa2a03a8a5099af2530940af3ecac36f5e3b
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-02-20 11:32:42 +0700

    error count box relasi space

--- app/Repositories/DashboardRepository.php
+++ app/Repositories/DashboardRepository.php
@@ -20,9 +20,9 @@ class DashboardRepository implements DashboardRepositoryInterface
     protected $shelves;
     protected $box;
     protected $order;
-    
+
     public function __construct(City $city, Area $area, Space $space, Shelves $shelves, Box $box, OrderDetail $order_detail)
-    {        
+    {
         $this->city      = $city;
         $this->area      = $area;
         $this->space     = $space;
@@ -39,7 +39,7 @@ class DashboardRepository implements DashboardRepositoryInterface
         $data = $data->count();
         return $data;
     }
-        
+
     public function countArea()
     {
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
@@ -61,7 +61,7 @@ class DashboardRepository implements DashboardRepositoryInterface
             $data = $data->where('areas.id', $admin->area_id);
         }
         $data = $data->where('spaces.deleted_at', NULL);
-        $data = $data->where('areas.deleted_at', NULL);    
+        $data = $data->where('areas.deleted_at', NULL);
         $data = $data->count();
         return $data;
     }
@@ -76,7 +76,7 @@ class DashboardRepository implements DashboardRepositoryInterface
         }
         $data = $data->where('spaces.status_id', 10);
         $data = $data->where('spaces.deleted_at', NULL);
-        $data = $data->where('areas.deleted_at', NULL);    
+        $data = $data->where('areas.deleted_at', NULL);
         $data = $data->count();
         return $data;
     }
@@ -85,13 +85,13 @@ class DashboardRepository implements DashboardRepositoryInterface
     {
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
         $data = $this->shelves->query();
-        $data = $data->leftJoin('spaces', 'spaces.id', '=', 'shelves.space_id');        
+        $data = $data->leftJoin('spaces', 'spaces.id', '=', 'shelves.space_id');
         $data = $data->leftJoin('areas', 'areas.id', '=', 'spaces.area_id');
         if(Auth::user()->roles_id == 2){
             $data = $data->where('areas.id', $admin->area_id);
         }
-        $data = $data->where('shelves.deleted_at', NULL);  
-        $data = $data->where('areas.deleted_at', NULL);     
+        $data = $data->where('shelves.deleted_at', NULL);
+        $data = $data->where('areas.deleted_at', NULL);
         $data = $data->count();
         return $data;
     }
@@ -101,13 +101,12 @@ class DashboardRepository implements DashboardRepositoryInterface
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
         $data = $this->box->query();
         $data = $data->leftJoin('shelves', 'shelves.id', '=', 'boxes.shelves_id');
-        $data = $data->leftJoin('spaces', 'spaces.id', '=', 'shelves.space_id');        
-        $data = $data->leftJoin('areas', 'areas.id', '=', 'spaces.area_id');
+        $data = $data->leftJoin('areas', 'areas.id', '=', 'shelves.area_id');
         if(Auth::user()->roles_id == 2){
             $data = $data->where('areas.id', $admin->area_id);
         }
-        $data = $data->where('boxes.deleted_at', NULL); 
-        $data = $data->where('areas.deleted_at', NULL);     
+        $data = $data->where('boxes.deleted_at', NULL);
+        $data = $data->where('areas.deleted_at', NULL);
         $data = $data->count();
         return $data;
     }
@@ -117,42 +116,42 @@ class DashboardRepository implements DashboardRepositoryInterface
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
         $data = $this->box->query();
         $data = $data->leftJoin('shelves', 'shelves.id', '=', 'boxes.shelves_id');
-        $data = $data->leftJoin('spaces', 'spaces.id', '=', 'shelves.space_id');        
+        $data = $data->leftJoin('spaces', 'spaces.id', '=', 'shelves.space_id');
         $data = $data->leftJoin('areas', 'areas.id', '=', 'spaces.area_id');
         if(Auth::user()->roles_id == 2){
             $data = $data->where('areas.id', $admin->area_id);
         }
-        $data = $data->where('boxes.deleted_at', NULL);    
-        $data = $data->where('areas.deleted_at', NULL);          
-        $data = $data->where('boxes.status_id', 10); 
+        $data = $data->where('boxes.deleted_at', NULL);
+        $data = $data->where('areas.deleted_at', NULL);
+        $data = $data->where('boxes.status_id', 10);
         $data = $data->count();
         return $data;
     }
-   
+
     public function totalSales(){
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
-        $data = $this->order_detail->query();     
-        $data = $data->select(DB::raw('SUM(order_details.amount) as total'));  
-        $data = $data->leftJoin('orders', 'orders.id', '=', 'order_details.order_id'); 
+        $data = $this->order_detail->query();
+        $data = $data->select(DB::raw('SUM(order_details.amount) as total'));
+        $data = $data->leftJoin('orders', 'orders.id', '=', 'order_details.order_id');
         $data = $data->leftJoin('areas', 'areas.id', '=', 'orders.area_id');
         if(Auth::user()->roles_id == 2){
             $data = $data->where('areas.id', $admin->area_id);
         }
-        $data = $data->where('orders.deleted_at', NULL);   
+        $data = $data->where('orders.deleted_at', NULL);
         $data = $data->first();
         return $data;
     }
 
     public function totalSalesMonth(){
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
-        $data = $this->order_detail->query();     
-        $data = $data->select(DB::raw('SUM(order_details.amount) as total'));   
+        $data = $this->order_detail->query();
+        $data = $data->select(DB::raw('SUM(order_details.amount) as total'));
         $data = $data->leftJoin('orders', 'orders.id', '=', 'order_details.order_id');
         $data = $data->leftJoin('areas', 'areas.id', '=', 'orders.area_id');
         if(Auth::user()->roles_id == 2){
             $data = $data->where('areas.id', $admin->area_id);
         }
-        $data = $data->where('orders.deleted_at', NULL);   
+        $data = $data->where('orders.deleted_at', NULL);
         $data = $data->whereRaw("MONTH(orders.created_at) = " . date('m'));
         $data = $data->first();
         return $data;
@@ -160,8 +159,8 @@ class DashboardRepository implements DashboardRepositoryInterface
 
     public function getGraphicOrder(){
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
-        $data = $this->order_detail->query();     
-        $data = $data->select(DB::raw('DATEPART(Year, order_details.created_at) Year'), DB::raw('DATEPART(Month, order_details.created_at) Month'), DB::raw('SUM(amount) [TotalAmount]'));   
+        $data = $this->order_detail->query();
+        $data = $data->select(DB::raw('DATEPART(Year, order_details.created_at) Year'), DB::raw('DATEPART(Month, order_details.created_at) Month'), DB::raw('SUM(amount) [TotalAmount]'));
         $data = $data->leftJoin('orders', 'orders.id', '=', 'order_details.order_id');
         $data = $data->leftJoin('areas', 'areas.id', '=', 'orders.area_id');
         if(Auth::user()->roles_id == 2){
