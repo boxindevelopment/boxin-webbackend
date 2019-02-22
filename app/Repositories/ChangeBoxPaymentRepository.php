3b3648baf29c62fd42b71ac962a661147c3b8471
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-02-22 14:44:25 +0700

    change box payment image

--- app/Repositories/ChangeBoxPaymentRepository.php
+++ app/Repositories/ChangeBoxPaymentRepository.php
@@ -21,16 +21,16 @@ class ChangeBoxPaymentRepository implements ChangeBoxPaymentRepositoryInterface
     {
         return $this->model->findOrFail($id);
     }
-    
+
     public function all()
     {
         $admin = AdminArea::where('user_id', Auth::user()->id)->first();
         $data = $this->model->query();
-        $data = $data->select('change_box_payments.id', 'change_box_payments.order_detail_id', 'change_box_payments.status_id', 'change_box_payments.id_name', 'change_box_payments.created_at', 'users.first_name',  'users.last_name');
-        $data = $data->leftJoin('order_details','order_details.id','=','change_box_payments.order_detail_id');      
+        $data = $data->select('change_box_payments.id', 'change_box_payments.image_transfer', 'change_box_payments.order_detail_id', 'change_box_payments.status_id', 'change_box_payments.id_name', 'change_box_payments.created_at', 'users.first_name',  'users.last_name');
+        $data = $data->leftJoin('order_details','order_details.id','=','change_box_payments.order_detail_id');
         $data = $data->leftJoin('users','users.id','=','change_box_payments.user_id');
         if(Auth::user()->roles_id == 2){
-            $data = $data->leftJoin('orders','orders.id','=','order_details.order_id');      
+            $data = $data->leftJoin('orders','orders.id','=','order_details.order_id');
             $data = $data->where('orders.area_id', $admin->area_id);
         }
         $data = $data->orderBy('change_box_payments.status_id', 'DESC')->orderBy('id', 'ASC');
@@ -42,18 +42,18 @@ class ChangeBoxPaymentRepository implements ChangeBoxPaymentRepositoryInterface
     {
         $data = $this->model->query();
         $data = $data->select('change_box_payments.*', 'users.first_name',  'users.last_name');
-        $data = $data->leftJoin('order_details','order_details.id','=','change_box_payments.order_detail_id');          
+        $data = $data->leftJoin('order_details','order_details.id','=','change_box_payments.order_detail_id');
         $data = $data->leftJoin('users','users.id','=','change_box_payments.user_id');
         $data = $data->where('change_box_payments.id', $id);
         $data = $data->get();
         return $data;
     }
-    
+
     public function create(array $data)
     {
         return $this->model->create($data);
     }
-    
+
     public function update(ChangeBoxPayment $pay, $data)
     {
         return $pay->update($data);
