d82ad9b5feb90fdbb1b7048c33d572e35ef891fe
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-02-22 10:22:52 +0700

    pay

--- app/Model/Payment.php
+++ app/Model/Payment.php
@@ -10,12 +10,12 @@ class Payment extends Model
     protected $table = 'payments';
 
     protected $fillable = [
-        'order_id', 
-        'user_id', 
-        'payment_type', 
-        'bank', 
-        'amount', 
-        'status_payment', 
+        'order_id',
+        'user_id',
+        'payment_type',
+        'bank',
+        'amount',
+        'status_payment',
         'status_id',
         'image_transfer',
         'id_name'
@@ -40,15 +40,11 @@ class Payment extends Model
     {
       $DEV_URL = 'https://boxin-dev-order.azurewebsites.net/images/payment/order/';
       $PROD_URL = 'https://boxin-prod-order.azurewebsites.net/images/payment/order/';
-      
+
       $url = (env('DB_DATABASE') == 'coredatabase') ? $DEV_URL : $PROD_URL;
 
       $image = $this->image_transfer;
-<<<<<<< HEAD
       $image_source = $url . $image;
-=======
-      $image_source = asset('images/payment/order') . '/' . $image;
->>>>>>> 5932553c52173cfcda2f9216fda98cc055e3cf56
       return $image_source;
     }
 
