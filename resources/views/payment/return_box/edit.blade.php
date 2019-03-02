6389c570ef90b18c674217f0d6768c9c78147756
Wawan Randianto <wawanrandianto1@gmail.com>
2019-03-02 20:57:10 +0700

    return box image pada detail sudah tampil

--- resources/views/payment/return_box/edit.blade.php
+++ resources/views/payment/return_box/edit.blade.php
@@ -40,16 +40,13 @@
                 @method('PUT')
                 <div class="row el-element-overlay">
                   @foreach ($data as $key => $value)
-                  @php
-                    $url = 'https://boxin-dev-order.azurewebsites.net/images/payment/return/';
-                  @endphp
                     <div class="col-md-6">                      
                         <div class="card">
                             <div class="el-card-item">
-                                <div class="el-card-avatar el-overlay-1"> <img src="{{$url}}{{ $value->image_transfer }}" alt="user" />
+                                <div class="el-card-avatar el-overlay-1"> <img src="{{ $value->image }}" alt="user" />
                                     <div class="el-overlay">
                                         <ul class="el-info">
-                                            <li><a class="btn default btn-outline image-popup-vertical-fit" href="{{$url}}{{ $value->image_transfer }}"><i class="icon-magnifier"></i></a></li>
+                                            <li><a class="btn default btn-outline image-popup-vertical-fit" href="{{ $value->image }}"><i class="icon-magnifier"></i></a></li>
                                         </ul>
                                     </div>
                                 </div>
