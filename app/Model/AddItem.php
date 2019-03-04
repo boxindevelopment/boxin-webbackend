0e7a23c7008f3a40ea159a92800790aba0d8a18f
Wawan Randianto <wawanrandianto1@gmail.com>
2019-03-04 11:12:24 +0700

    fix path image add item

--- app/Model/AddItem.php
+++ app/Model/AddItem.php
@@ -28,8 +28,8 @@ class AddItem extends Model
 
     public function getImageAttribute()
     {
-      $DEV_URL = 'https://boxin-dev-order.azurewebsites.net/images/detail_item_box/additem/';
-      $PROD_URL = 'https://boxin-prod-order.azurewebsites.net/images/detail_item_box/additem/';
+      $DEV_URL = 'https://boxin-dev-order.azurewebsites.net/images/detail_item_box/';
+      $PROD_URL = 'https://boxin-prod-order.azurewebsites.net/images/detail_item_box/';
       $url = (env('DB_DATABASE') == 'coredatabase') ? $DEV_URL : $PROD_URL;
       $image = $this->item_image;
       return $url . $image;
